7c6a62501ab19e3ff045393d2fb45db69a967420
Nicola Worthington <nicolaw@tfb.net>
2006-10-24 14:54:25 +0000

    git-svn-id: file:///var/tmp/data/svnroot/Parse-DMIDecode@810 610ec7d1-1c12-4784-ad90-13d12571cf29

--- examples/probe_machines.pl
+++ examples/probe_machines.pl
@@ -1,18 +1,65 @@
 #!/usr/bin/perl -wT
+############################################################
+#
+#   $Id: probe_machine.pl 809 2006-10-22 12:47:45Z nicolaw $
+#   probe_machine.pl - Example script for Parse::DMIDecode
+#
+#   Copyright 2006 Nicola Worthington
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+############################################################
+# vim:ts=4:sw=4:tw=78
 
 # Database credentials
-use constant DBI_DSN     => 'DBI:mysql:database:localhost';
+use constant DBI_DSN     => 'DBI:mysql:database:hostname';
 use constant DBI_USER    => 'username';
 use constant DBI_PASS    => 'password';
 
 # Where and how to generate a list of hosts to probe
-use constant HOSTS_REGEX => qr{([a-zA-|0-9\-\_\.]+)};
-use constant HOSTS_SKIP  => qw{(winfs|windows|w2kfs)};
+use constant HOSTS_REGEX => qr{(\S+)};
+use constant HOSTS_SKIP  => qw{(localhost|localdomain|127.0.0.1)};
 use constant HOSTS_SRC   => '/etc/hosts';
 
 # Remote command to gather information
-use constant SSH_CMD     => 'ssh -o BatchMode=yes -o ConnectTimeout=6 -o StrictHostKeyChecking=no -o PreferredAuthentications=publickey -o CheckHostIP=no -l root -i ~/.ssh/id_dsa_root';
-use constant REMOTE_CMD  => 'echo ==dmidecode==; dmidecode; echo ==ifconfig==; ifconfig -a; echo ==distribution==; egrep . /etc/debian_version /etc/redhat-release; echo ==listening==; netstat -ltnu; echo ==routingtable==; netstat -rn; echo ==x86info==; x86info; echo ==lspci==; lspci';
+use constant SSH_CMD     => 'ssh';
+use constant REMOTE_CMD  => 'export PATH=/bin:/usr/bin:/sbin:/usr/sbin;
+	echo ==dmidecode==; dmidecode;
+	echo ==ifconfig==; ifconfig -a;
+	echo ==distribution==; grep . /etc/debian_version /etc/redhat-release /etc/mandrake-release /etc/SuSE-release;
+	echo ==netstat==; netstat -ltnu;
+	echo ==routee==; route -n;
+	echo ==x86info==; x86info;
+	echo ==lspci==; lspci;
+	echo ==cpuinfo==; cat /proc/cpuinfo;
+	echo ==meminfo==; cat /proc/meminfo;
+	echo ==hostname==; hostname;
+	echo ==ide==; grep -r . /proc/ide/;
+	echo ==scsi==; grep -r . /proc/scsi/;
+	echo ==ipdiscover==; ipdiscover;
+	echo ==uname==; uname -a;
+	echo ==biosdecode==; biosdecode;
+	echo ==vpddecode==; vpddecode;
+	echo ==rpm==; rpm -qa --queryformat \"%{NAME} %{VERSION} %{SUMMARY}\n\";
+	echo ==dpkg==; dpkg -l;
+	echo ==resolv==; cat /etc/resolv.conf;
+	echo ==uptime==; cat /proc/uptime;
+	echo ==who==; who;
+	echo ==last==; last;
+	echo ==df==; df -TP';
+
+
+
 
 
 #########################################################
@@ -24,27 +71,34 @@ use constant REMOTE_CMD  => 'echo ==dmidecode==; dmidecode; echo ==ifconfig==; i
 #########################################################
 
 
+
+
 use 5.6.1;
 use strict;
 use DBI qw();
-use Parse::DMIDecode qw();
+use Getopt::Std qw(getopts);
+use Parse::DMIDecode 0.02 qw();
 
 %ENV = (PATH => '/bin:/usr/bin');
 $|++;
 
+my $opts = {};
+Getopt::Std::getopts('hCH:',$opts);
+display_help(),exit if defined $opts->{h};
 
 my $dbh = DBI->connect(DBI_DSN,DBI_USER,DBI_PASS,{AutoCommit => 0});
-create_tables() if grep(/createTables?/i,@ARGV);
+create_tables() if defined $opts->{C};
 
 my $dmi = Parse::DMIDecode->new(nowarnings => 1);
 my $skip_regex = HOSTS_SKIP;
+my @hosts = defined $opts->{H} ? ($opts->{H}) : get_hostnames();
 
-for my $machine (sort(get_hostnames())) {
-	print "Prcessing $machine ". "." x (50-length($machine));
+for my $machine (sort(@hosts)) {
+	print "Processing $machine ". "." x (49-length($machine));
 	print_result('skipped'), next if $machine =~ /$skip_regex/;
 
 	my $data = probe_server($machine);
-	if (defined $data->{'failed-to-connect'}) {
+	if (defined $data->{NOCONNECT}) {
 		print_result('connect failed');
 	} elsif (defined $data->{'system-uuid'} && $data->{'system-uuid'} =~ /^[A-F0-9\-]{36}$/) {
 		update_database($data);
@@ -69,17 +123,16 @@ sub print_result {
 
 
 sub probe_server {
-	my $machine = shift;
-
-	my $cmd = sprintf('%s %s "%s" 2>/dev/null', SSH_CMD, $machine, REMOTE_CMD);
-	my %raw = (hostname => $machine, 'failed-to-connect' => 1);
+	my ($machine) = $_[0] =~ /([a-z0-9\.\-\_]+)/i;
+	(my $cmd = sprintf('%s %s "%s" 2>/dev/null', SSH_CMD, $machine, REMOTE_CMD)) =~ s/\n//g;
+	my %raw = (HOSTNAME => $machine, NOCONNECT => 1);
 	my $group;
 
 	if (open(PH,'-|',$cmd)) {
 		while (local $_ = <PH>) {
 			if (/^==+(\S+?)==+$/) {
 				$group = $1;
-				delete $raw{'failed-to-connect'};
+				delete $raw{NOCONNECT};
 			} elsif (defined $group && $group =~ /\S+/) {
 				$raw{$group} .= $_;
 			} else {
@@ -89,10 +142,9 @@ sub probe_server {
 		close(PH);
 	}
 
-	return \%raw if defined $raw{'failed-to-connect'};
+	return \%raw if defined $raw{NOCONNECT};
 	return parse_raw_data($machine,\%raw);
 }
-
 
 
 sub parse_raw_data {
@@ -101,19 +153,55 @@ sub parse_raw_data {
 	# ==dmidecode==
 	if (defined $raw->{dmidecode}) {
 		$dmi->parse($raw->{dmidecode});
-		for (qw(system-uuid system-serial-number system-manufacturer system-product-name)) {
+		for (qw(system-uuid system-serial-number system-manufacturer system-product-name
+			system-vendor system-product baseboard-product-name baseboard-manufacturer
+			bios-version bios-vendor chassis-type)) {
 			$raw->{$_} = $dmi->keyword($_);
+			$raw->{$_} = '' unless defined $raw->{$_};
 		}
 		$raw->{'physical-cpu-qty'} = 0;
 		for my $handle ($dmi->get_handles(group => 'processor')) {
-			next unless $handle->keyword('processor-type') =~ /Central Processor/i;
+			next unless defined $handle->keyword('processor-type') &&
+					$handle->keyword('processor-type') =~ /Central Processor/i;
 			$raw->{'physical-cpu-qty'}++;
-			for (qw(processor-family processor-manufacturer processor-current-speed
+			for (qw(processor-family processor-manufacturer processor-current-speed processor-id
 				processor-type processor-version processor-signature processor-flags)) {
 				my $value = $handle->keyword($_);
-				$value = undef if defined $value && $value =~ /Not Specified/i;
-				$raw->{$_} = $value if !defined $raw->{$_};
+				if (!defined $value || (defined $value && $value =~ /Not Specified/i)) {
+					$raw->{$_} = '';
+				} else {
+					$raw->{$_} = $value unless defined $raw->{$_} && $raw->{$_} =~ /\S/;
+				}
 			}
+		}
+
+		# Account for older versions of dmidecode output
+		if ($raw->{'system-product'} =~ /\S/ && $raw->{'system-product-name'} !~ /\S/) {
+			$raw->{'system-product-name'} = $raw->{'system-product'};
+		}
+		if ($raw->{'system-vendor'} =~ /\S/ && $raw->{'system-manufacturer'} !~ /\S/) {
+			$raw->{'system-manufacturer'} = $raw->{'system-vendor'};
+		}
+
+		# Bodge together a pretend uuid
+		if (!defined($raw->{'system-uuid'}) || $raw->{'system-uuid'} !~ /^[A-F0-9\-]{36}$/) {
+			($raw->{'system-uuid'} = uc(join('-',
+					($raw->{'processor-id'}||''),
+					($raw->{'baseboard-product-name'}||''),
+					($raw->{'baseboard-manufacturer'}||''),
+					($raw->{'bios-version'}||''),
+					($raw->{'bios-vendor'}||''),
+					($raw->{'processor-signature'}||''),
+					($raw->{'processor-type'}||''),
+					($machine x 20),
+				))) =~ s/[^A-F0-9]//g;
+			$raw->{'system-uuid'} = sprintf('%s-%s-%s-%s-%s',
+					substr($raw->{'system-uuid'},0,8),
+					substr($raw->{'system-uuid'},7,4),
+					substr($raw->{'system-uuid'},11,4),
+					substr($raw->{'system-uuid'},15,4),
+					substr($raw->{'system-uuid'},19,12),
+				);
 		}
 	}
 
@@ -131,6 +219,12 @@ sub parse_raw_data {
 	if (defined $raw->{distribution}) {
 		if ($raw->{distribution} =~ /Red Hat Enterprise Linux/m) {
 			$raw->{distribution} = 'RHEL';
+		} elsif ($raw->{distribution} =~ /Slackware/) {
+			$raw->{distribution} = 'Slackware';
+		} elsif ($raw->{distribution} =~ /Mandrake/) {
+			$raw->{distribution} = 'Mandrake';
+		} elsif ($raw->{distribution} =~ /SuSE/) {
+			$raw->{distribution} = 'SuSE';
 		} elsif ($raw->{distribution} =~ /Ubuntu/) {
 			$raw->{distribution} = 'Ubuntu';
 		} elsif ($raw->{distribution} =~ /Red\s*Hat/) {
@@ -146,15 +240,14 @@ sub parse_raw_data {
 }
 
 
-
 sub create_tables {
 	my @statements;
 	my $statement;
 
 	while (local $_ = <DATA>) {
 		chomp;
-		next if /^\s*(#|\-\-|;)/;
-		last if /^__END__$/;
+		next if /^\s*(#|\-\-|;)/ || /^\s*$/;
+		last if /^__END__\s*$/;
 		s/\t/ /;
 
 		if (/;\s*$/) {
@@ -166,24 +259,28 @@ sub create_tables {
 		}
 	}
 
+	print "Recreating database tables ...";
 	for my $statement (@statements) {
 		$statement =~ s/;\s*//;
 		my $sth = $dbh->prepare($statement);
 		$sth->execute;
 	}
 	$dbh->commit;
+	print " done\n";
 }
 
 
-
 sub get_model_id {
-	my ($make,$model) = @_;
+	my ($make,$model,$data) = @_;
+	$make ||= 'Unknown';
+	$model ||= 'Unknown';
+
 	my $sth = $dbh->prepare('SELECT model_id FROM model WHERE make = ? AND model = ?');
 	$sth->execute($make,$model);
 	my ($model_id) = $sth->rows == 1 ? $sth->fetchrow_array : undef;
 
 	if (!defined $model_id && $sth->rows <= 1) {
-		my $form = undef;
+		my $form = (defined $data->{'chassis-type'} ? $data->{'chassis-type'} : undef);
 		if ($make =~ /^Dell (Inc\.|Computer Corporation)$/ && $model =~ /^PowerEdge (?:750|([12])\d\d0)$/) {
 			$form = defined $1 ? "${1}U" : '1U';
 		}
@@ -197,82 +294,158 @@ sub get_model_id {
 }
 
 
+sub update_record {
+	my $ref = {@_};
+	die "No table defined." if !exists $ref->{table};
+	die "No column data defined." if !exists $ref->{cols};
+	die "No where clause defined." if !exists $ref->{where};
+
+	# Delete or check for existing row
+	my @where; my @where_bind;
+	while (my ($col,$value) = each %{$ref->{where}}) {
+		push @where, sprintf(' %s = ? ',$col);
+		push @where_bind, $value;
+	}
+	my $sql = sprintf('%s FROM %s WHERE %s',
+			($ref->{delete_first} ? 'DELETE' : 'SELECT *'),
+			$ref->{table},
+			join(' AND ',@where),
+		);
+	my $sth = $dbh->prepare($sql);
+	$sth->execute(@where_bind);
+
+
+	# Update an existing row
+	if (!$ref->{delete_first} && $sth->rows >= 1) {
+		my @set; my @set_bind;
+		while (my ($col,$value) = each %{$ref->{cols}}) {
+			push @set, sprintf(' %s = ? ',$col);
+			push @set_bind, $value;
+		}
+		$sql = sprintf('UPDATE %s SET %s WHERE %s',
+				$ref->{table},
+				join(', ',@set),
+				join(' AND ',@where),
+			);
+		$sth = $dbh->prepare($sql);
+		$sth->execute(@set_bind,@where_bind);
+
+	# Insert a new row
+	} else {
+		my @cols; my @cols_bind; my @placeholders;
+		while (my ($col,$value) = each %{$ref->{cols}}) {
+			push @cols, $col;
+			push @cols_bind, $value;
+			push @placeholders, '?';
+		}
+		$sql = sprintf('INSERT INTO %s (%s) VALUES (%s)',
+				$ref->{table},
+				join(',',@cols),
+				join(',',@placeholders),
+			);
+		$sth = $dbh->prepare($sql);
+		$sth->execute(@cols_bind);
+	}
+
+	$sth->finish;
+}
+
 
 sub update_database {
 	my $data = shift;
 
-	my $model_id = get_model_id($data->{'system-manufacturer'},$data->{'system-product-name'});
+	my $model_id = get_model_id(
+			$data->{'system-manufacturer'},
+			$data->{'system-product-name'},
+			$data
+		);
 
-	my $sth = $dbh->prepare('SELECT COUNT(*) FROM machine WHERE uuid = ?');
-	$sth->execute($data->{'system-uuid'});
-	my ($exists) = $sth->fetchrow_array;
+	update_record(	table => 'machine',
+			cols => {
+				model_id => $model_id,
+				serial => $data->{'system-serial-number'},
+				uuid => $data->{'system-uuid'},
+			},
+			where => {
+				uuid => $data->{'system-uuid'},
+			},
+		);
+	
+	update_record(	table => 'host',
+			cols => {
+				uuid => $data->{'system-uuid'},
+				hostname => $data->{HOSTNAME},
+				os => $data->{'distribution'},
+			},
+			where => {
+				hostname => $data->{HOSTNAME},
+			},
+		);
 
-	if ($exists && defined $model_id) {
-		$sth = $dbh->prepare('UPDATE machine SET model_id = ?, serial = ? WHERE uuid = ?');
-		$sth->execute($model_id,$data->{'system-serial-number'},$data->{'system-uuid'});
-	} else {
-		$sth = $dbh->prepare('INSERT INTO machine (uuid,serial,model_id) VALUES (?,?,?)');
-		$sth->execute($data->{'system-uuid'},$data->{'system-serial-number'},$model_id);
+	my @probes = REMOTE_CMD =~ /==([a-z0-9\-\_]+)==/g;
+	for my $probe (@probes) {
+		update_record(	table => 'probe',
+				cols => {
+					uuid => $data->{'system-uuid'},
+					probe => $probe,
+					data => $data->{$probe},
+				},
+				where => {
+					uuid => $data->{'system-uuid'},
+					probe => $probe,
+				},
+			);
 	}
 
-	$sth = $dbh->prepare('SELECT hostname FROM host WHERE hostname = ?');
-	$sth->execute($data->{hostname});
-	if ($sth->rows) {
-		my $sth = $dbh->prepare('UPDATE host SET uuid = ?, os = ? WHERE hostname = ?');
-		$sth->execute($data->{'system-uuid'},$data->{distribution},$data->{hostname});
-	} else {
-		my $sth = $dbh->prepare('INSERT INTO host (hostname,uuid,os) VALUES (?,?,?)');
-		$sth->execute($data->{hostname},$data->{'system-uuid'},$data->{distribution});
-	}
+	my $sth = $dbh->prepare('DELETE FROM host WHERE uuid = ? AND hostname != ?');
+	$sth->execute($data->{'system-uuid'},$data->{HOSTNAME});
 
-	$sth = $dbh->prepare('SELECT uuid FROM probes WHERE uuid = ?');
-	$sth->execute($data->{'system-uuid'});
-	if ($sth->rows) {
-		my $sth = $dbh->prepare('UPDATE probes SET listening = ?, routes = ?, dmidecode = ? WHERE uuid = ?');
-		$sth->execute($data->{listening},$data->{routes},$data->{dmidecode},$data->{'system-uuid'});
-	} else {
-		my $sth = $dbh->prepare('INSERT INTO probes (listening,routes,dmidecode,uuid) VALUES (?,?,?,?)');
-		$sth->execute($data->{listening},$data->{routes},$data->{dmidecode},$data->{'system-uuid'});
-	}
-
-	$sth = $dbh->prepare('SELECT uuid FROM cpu WHERE uuid = ?');
-	$sth->execute($data->{'system-uuid'});
-	my ($processor_current_speed) = $data->{'processor-current-speed'} =~ /(\d+)/;
-	my @processor_flags = ();
-	for (@{$data->{'processor-flags'}}) {
-		if (/^(\S+)/) { push @processor_flags, $1; }
-	}
-	my $processor_flags = join(',',@processor_flags);
-	my @processor_bindings = ($data->{'processor-manufacturer'},$data->{'processor-family'},
-			$data->{'processor-version'},$processor_current_speed,$data->{'processor-signature'},
-			$processor_flags,$data->{'physical-cpu-qty'},$data->{'system-uuid'});
-	if ($sth->rows) {
-		my $sth = $dbh->prepare('UPDATE cpu SET manufacturer = ?, family = ?,
-			version = ?, speed = ?, signature = ?, flags = ?, qty = ? WHERE uuid = ?');
-		$sth->execute(@processor_bindings);
-	} else {
-		my $sth = $dbh->prepare('INSERT INTO cpu (manufacturer,family,version,speed,
-			signature,flags,qty,uuid) VALUES (?,?,?,?,?,?,?,?)');
-		$sth->execute(@processor_bindings);
-	}
-
-	$sth = $dbh->prepare('DELETE FROM host WHERE uuid = ? AND hostname != ?');
-	$sth->execute($data->{'system-uuid'},$data->{hostname});
-
-	$sth = $dbh->prepare('DELETE FROM nic WHERE uuid = ?');
-	$sth->execute($data->{'system-uuid'});
+	my %seen_hwaddr;
 	for (@{$data->{hwaddr}}) {
 		my ($hwaddr,$interface) = split(/\s+/,$_);
-		next if $interface =~ /:/;
 		$hwaddr =~ s/[^0-9a-f]+//gi;
-		$sth = $dbh->prepare('INSERT INTO nic (hwaddr,uuid,interface) VALUES (?,?,?)');
-		$sth->execute($hwaddr,$data->{'system-uuid'},$interface);
+		next if $interface =~ /:/ || exists $seen_hwaddr{$hwaddr};
+		$seen_hwaddr{$hwaddr} = 1;
+		update_record(	table => 'nic',
+				cols => {
+					uuid => $data->{'system-uuid'},
+					hwaddr => $hwaddr,
+					interface => $interface,
+				},
+				where => {
+					uuid => $data->{'system-uuid'},
+					hwaddr => $hwaddr,
+				},
+			);
 	}
+
+	my ($cpu_speed) = ($data->{'processor-current-speed'}||'') =~ /(\d+)/;
+	my @processor_flags = ();
+	if (ref($data->{'processor-flags'}) eq 'ARRAY') {
+		for (@{$data->{'processor-flags'}}) {
+			if (/^(\S+)/) { push @processor_flags, $1; }
+		}
+	}
+
+	update_record(	table => 'cpu',
+			cols => {
+				manufacturer => $data->{'processor-manufacturer'},
+				family => $data->{'processor-family'},
+				version => $data->{'processor-version'},
+				speed => $cpu_speed,
+				signature => $data->{'processor-signature'},
+				flags => join(',',@processor_flags),
+				qty => $data->{'physical-cpu-qty'},
+				uuid => $data->{'system-uuid'},
+			},
+			where => {
+				uuid => $data->{'system-uuid'},
+			},
+		);
 
 	$sth->finish;
 	$dbh->commit;
 }
-
 
 
 sub get_hostnames {
@@ -301,28 +474,39 @@ sub get_hostnames {
 	}
 
 	my $hosts = scalar(keys %hosts) || 0;
-	print " found $hosts host".($hosts == 1 ? '' : 's').".\n";
+	print " found $hosts host".($hosts == 1 ? '' : 's')."\n";
 	return sort keys %hosts;
+}
+
+
+sub display_help {
+	print qq{Syntax: $0 [-h] [-H <hostname>] [-C]
+    -h              Display this help
+    -H <hostname>   Only probe <hostname>
+    -C              Recreate the database tables
+};
 }
 
 
 
 __DATA__
+
 DROP TABLE IF EXISTS nic;
 DROP TABLE IF EXISTS cpu;
-DROP TABLE IF EXISTS probes;
+DROP TABLE IF EXISTS probe;
 DROP TABLE IF EXISTS service;
 DROP TABLE IF EXISTS host;
 DROP TABLE IF EXISTS machine;
 DROP TABLE IF EXISTS model;
+
 #DROP TABLE IF EXISTS contact;
 #DROP TABLE IF EXISTS history;
 
 CREATE TABLE model (
 		model_id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-		make VARCHAR(32) NOT NULL,
-		model VARCHAR(32) NOT NULL,
-		form ENUM('1U','2U','3U','Desktop','MiniTower','MidiTower','FullTower')
+		make VARCHAR(32),
+		model VARCHAR(32),
+		form VARCHAR(16)
 	) ENGINE=InnoDB;
 
 CREATE TABLE machine (
@@ -335,7 +519,7 @@ CREATE TABLE machine (
 CREATE TABLE host (
 		hostname VARCHAR(32) NOT NULL PRIMARY KEY,
 		uuid CHAR(36) NOT NULL,
-		os ENUM('Debian','RedHat','RHEL','Ubuntu','SuSE','Windows','Linux'),
+		os ENUM('Debian','Mandrake','RedHat','RHEL','Ubuntu','Gentoo','Slackware','SuSE','Windows','Linux'),
 		FOREIGN KEY (uuid) REFERENCES machine(uuid) ON DELETE CASCADE
 	) ENGINE=InnoDB;
 
@@ -351,7 +535,7 @@ CREATE TABLE service (
 CREATE TABLE nic (
 		hwaddr CHAR(12) NOT NULL,
 		uuid CHAR(36) NOT NULL,
-		interface CHAR(4),
+		interface VARCHAR(8),
 		PRIMARY KEY (hwaddr,uuid),
 		FOREIGN KEY (uuid) REFERENCES machine(uuid) ON DELETE CASCADE
 	) ENGINE=InnoDB;
@@ -368,14 +552,35 @@ CREATE TABLE cpu (
 		FOREIGN KEY (uuid) REFERENCES machine(uuid) ON DELETE CASCADE
 	) ENGINE=InnoDB;
 
-CREATE TABLE probes (
-		uuid CHAR(36) NOT NULL PRIMARY KEY,
-		routes TEXT,
-		listening TEXT,
-		dmidecode TEXT,
+CREATE TABLE probe (
+		uuid CHAR(36) NOT NULL,
+		probe VARCHAR(16) NOT NULL,
+		data TEXT,
+		PRIMARY KEY (uuid,probe),
 		FOREIGN KEY (uuid) REFERENCES machine(uuid) ON DELETE CASCADE
 	) ENGINE=InnoDB;
 
 __END__
 
+BatchMode yes
+CheckHostIP no
+ConnectTimeout 6
+StrictHostKeyChecking no
+PreferredAuthentications publickey
+IdentityFile ~/.ssh/id_dsa_root
+IdentityFile ~/.ssh/id_dsa
+NoHostAuthenticationForLocalhost yes
+ConnectionAttempts 1
+PasswordAuthentication no
+ForwardAgent yes
+
+Host server1.acmecompany.com
+        Port 1033
+
+Host server2.acmecompany.com
+        User admin
+
+Host *
+        User root
+        Port 22
 
